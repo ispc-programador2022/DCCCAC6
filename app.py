63a621cfca3eeacb0f0f09bba3f30103ec93d07b
Juan Diego Coianiz <diego.c.coianiz@gmail.com>
2022-11-03 06:21:10 -0300

    problma para cargar nombres de ciudades en el input de la web, es el tema a solucionar

--- app.py
+++ app.py
@@ -7,23 +7,32 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
 import os
 from datetime import date
 from datetime import datetime
 clear = lambda: os.system('cls')
 
 options = ChromeOptions()
-options.headless = True
+#options.headless = True
 driver = webdriver.Chrome("C:\webDrivers\chromedriver.exe", options=options)
-
 clear()
-print("")
+
+area_list = ["Rosario (Rosario), Santa Fe", "Córdoba (Capital), Córdoba", "Capital Federal (Capital Federal), Capital Federal",
+"Paraná (Paraná), Entre Ríos", "Posadas (Capital), Misiones", "Formosa (Formosa), Formosa", "Salta (Capital), Salta",
+"Viedma (Adolfo Alsina), Río Negro", "Ushuaia (Ushuaia), Tierra del Fuego, Antártida e Islas del Atlántico Sur", 
+"Mendoza (Capital), Mendoza", "San Juan (Capital), San Juan", "San Luis (Capital), San Luis", "Rawson (Rawson), Chubut",
+"Santiago del Estero (Capital), Santiago del Estero", "La Rioja (Capital), La Rioja", "Neuquén (Confluencia), Neuquén"]
+
+
 #endregion
 #
 
-#region def()
+#region funciones()
 def get_Url(url):
-    # requiere la pág para escrapear
+    
+    # requiere la pág para escrapear mediante el metodo requests.get()
     try:
         return requests.get(url)
     except Exception as e:
@@ -33,17 +42,19 @@ def get_Url(url):
 #
 
 def get_divs( return_, link, typeOfDiv1, attrKey1, attrkValue1, typeOfDiv2 = 0, get = False, many = False, attrKey2 = 0, attrkValue2 = 0 ):
-    #recupera la división especifica para trabajar y return_orna Href of text
-        
+    
+    #activa o inive la funcion get_url() en base a necesitar => requests.get(TRUE) o driver.get("FALSE")     
     if get == True:
         webPage = get_Url(link)
         soup = bs(webPage.text, "lxml")
     else:
         soup = bs(link, "lxml")
+        driver.close()
 
+    #devuelve un array de elementos find_all() o un solo elemento find()
     if many:
         divisions = soup.find_all(typeOfDiv1, attrs = {attrKey1 : attrkValue1})
-        #print(divisions)
+
         if typeOfDiv2 != 0 :
             if attrKey2 != 0:
                 divisions = soup.find_all(typeOfDiv2, attrs = {attrKey2 : attrkValue2})
@@ -52,7 +63,7 @@ def get_divs( return_, link, typeOfDiv1, attrKey1, attrkValue1, typeOfDiv2 = 0, 
         
         if return_ == "href":
             return [div.a.get("href") for div in divisions]
-        elif ret == "text":
+        elif return_ == "text":
             return [div.a.get_text() for div in divisions]
     else:
         if typeOfDiv2 != 0:
@@ -78,15 +89,12 @@ def get_divs( return_, link, typeOfDiv1, attrKey1, attrkValue1, typeOfDiv2 = 0, 
             return soup.find(typeOfDiv1, attrs = {attrKey1 : attrkValue1})
 #
 
-def climate_objetc_creator(clima_list):
+def climate_objetc_creator(clima_list, area):
+   
+    # retorna un objeto con todas las propiedades recuperadas de la web listo para ser procesado en pandas
     new_object = {}
     new_object["id"] = datetime.now().strftime("%d%m%Y%H%M%S")
     new_object["fecha"] = datetime.now().strftime("%d/%h/%Y")
-    
-    area = input("area de recolección de datos? (nacional por defecto) : ")
-    if area == "":
-        area = "Nacional"
-    
     new_object["area"] = area
     max = 0
     min = 0
@@ -127,14 +135,47 @@ def climate_objetc_creator(clima_list):
     new_object["promedio_minimo"] = min_average//len(clima_list)
     new_object["temperatura_media"] = (new_object["promedio_maximo"] + new_object["promedio_minimo"]) / 2
 
-    print(new_object)
     return new_object
 
+def input_write(tagKey, tagValue, input):
+    
+    #escribe sobre un imput especfico de la web un comando solicitado
+    if tagKey == "ID":
+        tag = By.ID
+    elif tagKey == "NAME":
+        tag = By.NAME
+    elif tagKey == "XPATH":
+        tag = By.XPATH
+    elif tagKey == "CSS_SELECTOR":
+        tag = By.CSS_SELECTOR
 
+    element = driver.find_element(tag, tagValue)
+    element.send_keys(input, Keys.ARROW_DOWN)
+    driver.implicitly_wait(2)
+    try:
+        element.submit()
+    except Exception as e:
+        print("problema:\n")
+        print(e)
+    element.clear()
+#
+
+def scrape_cities():
+    print("")
+#
 #endregion
 #
 
 #region scrap_init
-url_pronostico_nacional = "https://www.smn.gob.ar/pronostico"
-driver.get(url_pronostico)
-climate_objetc_creator(get_divs("table", driver.page_source, "ul", "id", "lista_matriz", "li"))
+driver.get("http://www.google.com")
+input_write("NAME", "q", "hola")
+
+# url_pronostico_nacional = "https://www.smn.gob.ar/pronostico"
+# driver.get(url_pronostico_nacional)
+# input_write("NAME", "q", area_list[10])
+
+#for area in range(len(area_list)):
+# input_write("NAME", "q", area_list[5])
+# objeto = climate_objetc_creator(get_divs("table", driver.page_source, "ul", "id", "lista_matriz", "li"))
+# for key,value in objeto.items():
+#     print(f"{key}:{value}")
