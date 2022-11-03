ca8a78ecd7a4491e7bff7f3534b3306f95072ffd
Juan Diego Coianiz <diego.c.coianiz@gmail.com>
2022-11-03 17:25:38 -0300

    funciona todo el scrap, falta crear los csv con pandas

--- app.py
+++ app.py
@@ -6,18 +6,15 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
-from selenium.webdriver.common.by import By
-from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
+
 import os
 from datetime import date
 from datetime import datetime
 clear = lambda: os.system('cls')
 
 options = ChromeOptions()
-#options.headless = True
-driver = webdriver.Chrome("C:\webDrivers\chromedriver.exe", options=options)
-clear()
+options.headless = True
 
 area_list = ["Rosario (Rosario), Santa Fe", "Córdoba (Capital), Córdoba", "Capital Federal (Capital Federal), Capital Federal",
 "Paraná (Paraná), Entre Ríos", "Posadas (Capital), Misiones", "Formosa (Formosa), Formosa", "Salta (Capital), Salta",
@@ -25,6 +22,11 @@ area_list = ["Rosario (Rosario), Santa Fe", "Córdoba (Capital), Córdoba", "Cap
 "Mendoza (Capital), Mendoza", "San Juan (Capital), San Juan", "San Luis (Capital), San Luis", "Rawson (Rawson), Chubut",
 "Santiago del Estero (Capital), Santiago del Estero", "La Rioja (Capital), La Rioja", "Neuquén (Confluencia), Neuquén"]
 
+driver = webdriver.Chrome("C:\webDrivers\chromedriver.exe", options=options)
+url_pronostico_nacional = "https://www.smn.gob.ar/pronostico"
+driver.get(url_pronostico_nacional)
+  
+clear()
 
 #endregion
 #
@@ -49,7 +51,7 @@ def get_divs( return_, link, typeOfDiv1, attrKey1, attrkValue1, typeOfDiv2 = 0, 
         soup = bs(webPage.text, "lxml")
     else:
         soup = bs(link, "lxml")
-        driver.close()
+        #driver.close()
 
     #devuelve un array de elementos find_all() o un solo elemento find()
     if many:
@@ -93,9 +95,9 @@ def climate_objetc_creator(clima_list, area):
    
     # retorna un objeto con todas las propiedades recuperadas de la web listo para ser procesado en pandas
     new_object = {}
-    new_object["id"] = datetime.now().strftime("%d%m%Y%H%M%S")
+    new_object["id"] = datetime.now().strftime("%d%m%Y%H%M%S")+area
     new_object["fecha"] = datetime.now().strftime("%d/%h/%Y")
-    new_object["area"] = area
+    new_object["ciudad"] = area
     max = 0
     min = 0
     max_average = 0
@@ -112,7 +114,10 @@ def climate_objetc_creator(clima_list, area):
         min_day = clima_list[i][0] + ":" + value[4][0] + value[4][1] + "°"
         max_value = int(value[1][0] + value[1][1])
         max_average += max_value
-        min_value = int(value[4][0] + value[4][1])
+        if value[4][1] != "°":
+            min_value = int(value[4][0] + value[4][1])
+        else:
+            min_value = int(value[4][0])
         min_average += min_value
         if i == 0:
             max = max_value
@@ -136,46 +141,56 @@ def climate_objetc_creator(clima_list, area):
     new_object["temperatura_media"] = (new_object["promedio_maximo"] + new_object["promedio_minimo"]) / 2
 
     return new_object
+#
 
-def input_write(tagKey, tagValue, input):
-    
-    #escribe sobre un imput especfico de la web un comando solicitado
-    if tagKey == "ID":
-        tag = By.ID
-    elif tagKey == "NAME":
-        tag = By.NAME
-    elif tagKey == "XPATH":
-        tag = By.XPATH
-    elif tagKey == "CSS_SELECTOR":
-        tag = By.CSS_SELECTOR
+def input_write(input):
 
-    element = driver.find_element(tag, tagValue)
+    #hace uso del buscador para determinar que ciudades analizar
+    element = driver.find_element_by_xpath('/html/body/div[2]/div/section/div/section/div/div/div/div/div/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div[1]/input')
+    driver.implicitly_wait(2)
+
     element.send_keys(input, Keys.ARROW_DOWN)
+    button = driver.find_element_by_xpath('/html/body/div[2]/div/section/div/section/div/div/div/div/div/div/div[3]/div/div/div[1]/div[1]/div[2]/div[2]/ul/li[2]/a')
     driver.implicitly_wait(2)
     try:
-        element.submit()
+        button.click()
     except Exception as e:
         print("problema:\n")
         print(e)
-    element.clear()
+    element.clear()  
 #
 
-def scrape_cities():
-    print("")
+def city_name(area):
+
+    #recupera de la lista de areas el nombre de cada una, dejando de lado otra info que necesita el <input> buscador
+    city = ""
+    for text in area:
+        if text[0] == "(":
+            break
+        else:
+            city += text
+    return city
 #
+
+def scrape_cities(areas):
+    
+    data = []
+    for i in range(len(areas)):
+        input_write(areas[i])
+        city = city_name(areas[i])
+        print(f"inicia el proceso numero {i} con la ciudad de {city}")
+        objeto = climate_objetc_creator(get_divs("table", driver.page_source, "ul", "id", "lista_matriz", "li"), city)
+        data.append(objeto)
+        for key,value in objeto.items():
+            print(f"{key}:{value}")
+        print("\n")
+        driver.implicitly_wait(2)
+    
+    return data
+#
+
 #endregion
 #
 
 #region scrap_init
-driver.get("http://www.google.com")
-input_write("NAME", "q", "hola")
-
-# url_pronostico_nacional = "https://www.smn.gob.ar/pronostico"
-# driver.get(url_pronostico_nacional)
-# input_write("NAME", "q", area_list[10])
-
-#for area in range(len(area_list)):
-# input_write("NAME", "q", area_list[5])
-# objeto = climate_objetc_creator(get_divs("table", driver.page_source, "ul", "id", "lista_matriz", "li"))
-# for key,value in objeto.items():
-#     print(f"{key}:{value}")
+scrape_cities(area_list)
