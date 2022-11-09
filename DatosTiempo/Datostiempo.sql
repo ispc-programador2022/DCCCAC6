da465fb1ec56b3df4fdcd0801bef681c4d6db41f
Mariano Delfino <marianod2003@gmail.com>
2022-11-09 10:12:40 -0300

    agrego base de datos donde se van a guardar las temperaturas del pronostico y los datos reales actuales

---
+++ DatosTiempo/Datostiempo.sql
@@ -0,0 +1,42 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+CREATE SCHEMA IF NOT EXISTS `DatosTiempo` DEFAULT CHARACTER SET utf8 ;
+USE `DatosTiempo` ;
+CREATE TABLE IF NOT EXISTS `DatosTiempo`.`Pronostico` (
+  `Id ciudad` INT NOT NULL,
+  `Fecha` DATE NULL,
+  `Ciudad` VARCHAR(20) NULL,
+  `LunesMin` INT NULL,
+  `LunesMax` INT NULL,
+  `MartesMax` INT NULL,
+  `MartesMin` INT NULL,
+  `MiercolesMax` INT NULL,
+  `MiercolesMin` INT NULL,
+  `JuevesMax` INT NULL,
+  `ViernesMin` INT NULL,
+  `ViernesMax` INT NULL,
+  `SabadoMin` INT NULL,
+  `SabadoMax` INT NULL,
+  `DomingoMin` INT NULL,
+  `DomingoMax` INT NULL,
+  PRIMARY KEY (`Id ciudad`))
+ENGINE = InnoDB
+COMMENT = '	';
+
+CREATE TABLE IF NOT EXISTS `DatosTiempo`.`DatosReales` (
+  `id ciudades` INT NOT NULL,
+  `Fecha y hora` DATETIME NULL,
+  `Ciudad` VARCHAR(20) NULL,
+  `Temperatura` DECIMAL(1) NULL,
+  `Humedad` INT NULL,
+  `Presión` INT NULL,
+  `viento` INT NULL,
+  `visibilidad` INT NULL,
+  `estado del clima` VARCHAR(25) NULL,
+  PRIMARY KEY (`id ciudades`))
+ENGINE = InnoDB;
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
